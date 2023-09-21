79da634bef027d7699358e898a2ce27a34c8f560
joshuaSoftIQ <joshua.falodu.882@accesscreative.ac.uk>
2023-09-21 13:43:52 +0100

    adjusting the code

--- Jfront.py
+++ Jfront.py
@@ -1,5 +1,6 @@
 from softiqbackend import*
-
+from softiqturtlebackend import *
+'''
 while True:
     action = 0
     print("\n1. To enter new data", "\n2. To display the data","\n3. To exit the program")
@@ -21,3 +22,29 @@ while True:
         break
     else:
         pass
+'''
+
+while True:
+    print("\n1. To enter new data")
+    print("2. To display the data")
+    print("3. To exit the program")
+
+    action = input("Enter a number: ")
+
+    if action == '1':
+        name = input("Name: ")
+        age = input("Age: ")
+        hair = input("Hair Colour: ")
+        sex = input("Sex (Male/Female): ")
+        role = input("Role: ")
+        
+
+    elif action == '2':
+        
+        t = turtle.Turtle()
+
+        display_data = input("What do you want to select? ")
+        t.bye()
+
+    elif action == '3':
+        break
