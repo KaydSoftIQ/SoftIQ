e600b56a577904dab54cde08eb254c2cfa91b0bd
OliverSoftIQ <oliver.cooper-perrott.106@accesscreative.ac.uk>
2023-09-21 14:03:38 +0100

    Edited the backend database

--- softiqbackend.py
+++ softiqbackend.py
@@ -1,3 +1,23 @@
+
+#This is used for the frontend
+'''
+k = input("Action: ")
+
+if k == "Age":
+    i = int(input("Age: "))
+else:
+    i = input(":")
+'''
+
+
+
+class CollectingData:
+
+    def collecting(k,i):
+        print(test.count_data(k,i))
+
+
+
 class DataTable:
 
     def __init__(self):
@@ -8,8 +28,19 @@ class DataTable:
         self.data[name] = kwargs
         self.data[name]["Name"] = name
 
+    def count_data(self, data, value):
+        c=0
+
+        for i in self.data:
+            if self.data[i][data] == value:
+                c += 1
+        return c
+
         
 
 test = DataTable()
-test.insert_data("Joao", Age=16, County="London")
-print(test.data)
+test.insert_data("Joao", Age=16, HairColour="Black", Sex="Male", Role="Programmer")
+test.insert_data("Oliver", Age=16, HairColour="Brown", Sex="Male", Role="Programmer")
+test.insert_data("KayD", Age=19, HairColour="Black", Sex="Female", Role="Musician")
+CollectingData.collecting(k,i)
+#print(test.count_data("Age",16))
