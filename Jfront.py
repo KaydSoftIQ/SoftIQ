a5b77ecaa4827269bcd1faf3f402b4817b229b58
Joshua Falodu <joshua.falodu.882@accesscreative.ac.uk>
2023-09-21 10:16:30 +0100

    test

---
+++ Jfront.py
@@ -0,0 +1,18 @@
+
+while True:
+
+    print("1) Enter Expeness\n 2) Enter View Summary\n 3)Exit\n ")
+    choose = input("Choose 1, 2, 3: \n" )
+    if choose == "1":
+        name = input("Enter name: ")
+        age = input("Enter age: ")
+        hair_colour = input("Enter Hair Colour: ")
+        sex = input("Enter Sex: ")
+        role = input("Enter role: \n")
+        exit
+
+    elif choose == "2":
+        print("d")
+        break
+    else:
+        break
